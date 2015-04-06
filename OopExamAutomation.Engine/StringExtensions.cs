a2cb177c05718cdcf3b269ed0fc5191237f48570
Nikolay Kostov <admin@nikolay.it>
2015-04-06 11:20:09 +0300

    Added 2 useful string methods in OopExamAutomation.Engine: - GetStringBetween - GetUsername

---
+++ OopExamAutomation.Engine/StringExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace OopExamAutomation.Engine
+{
+    using System;
+    using System.IO;
+
+    public static class StringExtensions
+    {
+        public static string GetStringBetween(this string input, string startString, string endString, int startFrom = 0)
+        {
+            input = input.Substring(startFrom);
+            startFrom = 0;
+            if (!input.Contains(startString) || !input.Contains(endString))
+            {
+                return string.Empty;
+            }
+
+            var startPosition = input.IndexOf(startString, startFrom, StringComparison.Ordinal) + startString.Length;
+            if (startPosition == -1)
+            {
+                return string.Empty;
+            }
+
+            var endPosition = input.IndexOf(endString, startPosition, StringComparison.Ordinal);
+            if (endPosition == -1)
+            {
+                return string.Empty;
+            }
+
+            return input.Substring(startPosition, endPosition - startPosition);
+        }
+
+        private static string GetUsername(this string path)
+        {
+            var directoryName = Path.GetFileName(path);
+            var indexOfBracket = directoryName.IndexOf("(", StringComparison.Ordinal);
+            var username = directoryName.Substring(0, indexOfBracket).Trim();
+            return username;
+        }
+    }
+}
