94d6d1d95994bf84b79522a491695dce03e177dc
Nikolay Kostov <admin@nikolay.it>
2015-04-07 11:46:21 +0300

    Added ITest and PredicateTest (implementation of ITest)

---
+++ OopExamAutomation.Engine/ITest.cs
@@ -0,0 +1,9 @@
+﻿namespace OopExamAutomation.Engine
+{
+    using System.Reflection;
+
+    public interface ITest
+    {
+        decimal CalculatePoints(Assembly assembly);
+    }
+}
---
+++ OopExamAutomation.Engine/PredicateTest.cs
@@ -0,0 +1,30 @@
+﻿namespace OopExamAutomation.Engine
+{
+    using System;
+    using System.Reflection;
+
+    public class PredicateTest : ITest
+    {
+        private readonly decimal maxPoints;
+
+        private readonly Func<Assembly, bool> predicate;
+
+        public PredicateTest(decimal maxPoints, Func<Assembly, bool> predicate)
+        {
+            this.maxPoints = maxPoints;
+            this.predicate = predicate;
+        }
+
+        public decimal CalculatePoints(Assembly assembly)
+        {
+            if (this.predicate(assembly))
+            {
+                return this.maxPoints;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
