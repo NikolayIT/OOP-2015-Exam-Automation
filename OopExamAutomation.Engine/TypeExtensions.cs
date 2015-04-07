5746b78fcff148c9652d5d20d9cd6f6531bc9dbc
Nikolay Kostov <admin@nikolay.it>
2015-04-07 18:45:55 +0300

    Added few extension methods (helpers) for types Added TypeTest (implementation of ITest that works with Types instead of assemblies)

---
+++ OopExamAutomation.Engine/TypeExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace OopExamAutomation.Engine
+{
+    public static class TypeExtensions
+    {
+        public static object GetInstance(this Type type)
+        {
+            try
+            {
+                return Activator.CreateInstance(type);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static T GetPropertyValue<T>(this object source, string property)
+        {
+            try
+            {
+                var value = source.GetType().GetProperty(property).GetValue(source, null);
+                return (T)value;
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        public static bool CheckPropertyValue<T>(this Type type, string propertyName, T value)
+        {
+            var obj = type.GetInstance();
+            if (obj == null) return false;
+            return obj.GetPropertyValue<T>(propertyName).Equals(value);
+        }
+
+        public static bool CheckPropertyValue<T>(this Type type, string propertyName, Func<T, bool> checkValue)
+        {
+            var obj = type.GetInstance();
+            if (obj == null) return false;
+            var propertyValue = obj.GetPropertyValue<T>(propertyName);
+            if (propertyValue == null) return false;
+            return checkValue(propertyValue);
+        }
+    }
+}
