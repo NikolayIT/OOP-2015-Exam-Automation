c29204a13de878c9b82d708e2f27950b3a6a91f4
Nikolay Kostov <admin@nikolay.it>
2015-04-07 12:18:42 +0300

    Running tests implemented as static method in Program class (OopExamAutomation.Console)

--- OopExamAutomation.Console/Program.cs
+++ OopExamAutomation.Console/Program.cs
@@ -1,5 +1,14 @@
 ﻿namespace OopExamAutomation.Console
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    using OopExamAutomation.Engine;
+    using OopExamAutomation.Problems.ArmyOfCreatures;
+
     public static class Program
     {
         private static string workingDirectory = @"C:\OOP\";
@@ -7,8 +16,40 @@
         private static string reportsDirectory = workingDirectory + @"\reports\";
         private static string outputFile = workingDirectory + @"\results.csv";
 
+        private static ITestsProvider testsProvider = new ArmyOfCreaturesTestsProvider();
+
         public static void Main()
         {
+            IEnumerable<ITest> tests = testsProvider.GetTests();
+
+            // Extract
+            // Compile
+
+            var assembly = Assembly.LoadFile(@"C:\Temp\OOP\Solution\ArmyOfCreatures\bin\Debug\ArmyOfCreatures.exe");
+
+            var totalPoints = RunTests(tests, assembly, Console.Out);
+            Console.WriteLine(totalPoints);
+            // RunTests
+        }
+
+        private static decimal RunTests(IEnumerable<ITest> tests, Assembly assembly, TextWriter output)
+        {
+            decimal totalPoints = 0;
+
+            foreach (var test in tests)
+            {
+                var testPoints = RunTest(test, assembly, output);
+                totalPoints += testPoints;
+            }
+
+            return totalPoints;
+        }
+
+        private static decimal RunTest(ITest test, Assembly assembly, TextWriter output)
+        {
+            var result = test.CalculateResult(assembly);
+            output.WriteLine("{0} ({1} {2}) - {3}", test.Name, result.Points, result.Points == 1 ? "point" : "points", result.TextResult);
+            return result.Points;
         }
     }
 }
